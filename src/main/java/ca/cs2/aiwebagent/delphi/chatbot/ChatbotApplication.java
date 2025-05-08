02da93314dcd3c9cd13d4002554a057d41d42e4d
qasimali <ranaqasim@gmail.com>
2025-05-08 17:04:40 -0400

    Springboot project created

---
+++ src/main/java/ca/cs2/aiwebagent/delphi/chatbot/ChatbotApplication.java
@@ -0,0 +1,13 @@
+package ca.cs2.aiwebagent.delphi.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotApplication.class, args);
+    }
+
+}
